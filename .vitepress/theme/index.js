0099cb81d47d0c1ef63963a208625dc72a49b990
mikhaildadaev <mikhaildadaev@gmail.com>
2026-07-11 13:38:30 +0300

    Обновление скрипта

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,19 +1,19 @@
 import DefaultTheme from 'vitepress/theme'
-import CardPet from '../components/CardPet.vue'
-import CardVolunteer from '../components/CardVolunteer.vue'
+import CardHumanHero from '../components/CardHumanHero.vue'
+import CardPetHero from '../components/CardPetHero.vue'
 import GalleryPhoto from '../components/GalleryPhoto.vue'
 import ListGuardians from '../components/ListGuardians.vue'
+import ListHumans from '../components/ListHumans.vue'
 import ListPets from '../components/ListPets.vue'
-import ListVolunteers from '../components/ListVolunteers.vue'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
-    app.component('CardPet', CardPet)
-    app.component('CardVolunteer', CardVolunteer)
+    app.component('CardHumanHero', CardHumanHero)
+    app.component('CardPetHero', CardPetHero)
     app.component('GalleryPhoto', GalleryPhoto)
     app.component('ListGuardians', ListGuardians)
+    app.component('ListHumans', ListHumans)
     app.component('ListPets', ListPets)
-    app.component('ListVolunteers', ListVolunteers)
   }
 }
