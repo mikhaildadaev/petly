1cce8a2516a39027b1bf3fa8b0662d50d7acb763
mikhaildadaev <mikhaildadaev@gmail.com>
2026-07-11 12:39:00 +0300

    Обновление компонента

--- .vitepress/components/CardDogs.vue
+++ .vitepress/components/CardDogs.vue
@@ -1,28 +1,11 @@
 <template>
   <div class="hero-card">
-    <!-- Мета-информация -->
     <div class="hero-meta">
-      <span v-if="gender" class="tag gender-tag" :data-gender="gender">
-        {{ gender }}
-      </span>
-      <span v-if="age" class="tag age-tag">
-        {{ age }}
-      </span>
-      <span v-if="size" class="tag size-tag">
-        {{ size }}
-      </span>
+      <span v-if="gender" class="tag gender-tag" :data-gender="gender">{{ gender }}</span>
+      <span v-if="age" class="tag age-tag">{{ age }}</span>
+      <span v-if="size" class="tag size-tag">{{ size }}</span>
     </div>
-
-    <!-- Изображение -->
-    <img 
-      :src="image" 
-      :alt="name" 
-      class="hero-image"
-      loading="lazy"
-      @error="handleImageError"
-    />
-
-    <!-- Оверлей с именем -->
+    <img :src="image" :alt="name" class="hero-image" loading="lazy" @error="handleImageError"/>
     <div class="hero-overlay">
       <h1 class="name">{{ name }}</h1>
     </div>
@@ -32,16 +15,17 @@
 <script setup>
 import { computed } from 'vue'
 import { useData } from 'vitepress'
-const getFrontmatter = () => {
-  return frontmatter?.value || frontmatter || {}
-}
 
-const name = computed(() => getFrontmatter().title || 'Безымянный друг')
-const gender = computed(() => getFrontmatter().gender || '')
-const age = computed(() => getFrontmatter().age || '')
-const size = computed(() => getFrontmatter().size || '')
-const image = computed(() => getFrontmatter().image || '/placeholder-dog.svg')
+const { frontmatter } = useData()
 
+// Безопасное получение frontmatter
+const fm = computed(() => frontmatter?.value || frontmatter || {})
+
+const name = computed(() => fm.value.title || 'Безымянный друг')
+const gender = computed(() => fm.value.gender || '')
+const age = computed(() => fm.value.age || '')
+const size = computed(() => fm.value.size || '')
+const image = computed(() => fm.value.image || '/placeholder-dog.svg')
 
 // Обработчик ошибки загрузки изображения
 const handleImageError = (e) => {
