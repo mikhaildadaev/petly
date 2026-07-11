0e7d80476b1bf4d971c62dc28c3fe6b7823547b2
mikhaildadaev <mikhaildadaev@gmail.com>
2026-07-11 12:41:54 +0300

    Обновление компонентов

---
+++ .vitepress/components/CardVolunteer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hero-card">
+    <div class="hero-meta">
+      <span v-if="gender" class="tag gender-tag" :data-gender="gender">{{ gender }}</span>
+      <span v-if="age" class="tag age-tag">{{ age }}</span>
+      <span v-if="size" class="tag size-tag">{{ size }}</span>
+    </div>
+    <img :src="image" :alt="name" class="hero-image" loading="lazy" @error="handleImageError"/>
+    <div class="hero-overlay">
+      <h1 class="name">{{ name }}</h1>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useData } from 'vitepress'
+
+const { frontmatter } = useData()
+
+// Безопасное получение frontmatter
+const fm = computed(() => frontmatter?.value || frontmatter || {})
+
+const name = computed(() => fm.value.title || 'Безымянный друг')
+const gender = computed(() => fm.value.gender || '')
+const age = computed(() => fm.value.age || '')
+const size = computed(() => fm.value.size || '')
+const image = computed(() => fm.value.image || '/placeholder-dog.svg')
+
+// Обработчик ошибки загрузки изображения
+const handleImageError = (e) => {
+  e.target.src = '/placeholder-dog.svg'
+  e.target.alt = 'Изображение временно недоступно'
+}
+</script>
