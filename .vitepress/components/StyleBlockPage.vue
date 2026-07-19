f697254838475b85bdc02284318b99bff33cf8af
mikhaildadaev <mikhaildadaev@gmail.com>
2026-07-19 19:12:10 +0300

    Обновление компонентв

--- .vitepress/components/StyleBlockPage.vue
+++ .vitepress/components/StyleBlockPage.vue
@@ -1,33 +1,41 @@
 <template>
   <div class="style-block-page" @click="closeAllTooltips">
-    <div v-for="(block, blockIndex) in filteredBlocks" :key="blockIndex" class="block-section" :class="`block-type-${type}`">
-      <h2 class="block-title">{{ block.name }}</h2>
-      <div class="block-grid">
-        <div v-for="(item, itemIndex) in block.list" :key="itemIndex" class="block-item">
-          <div class="block-item-image">
-            <img :src="item.image" loading="lazy" :alt="item.description" />
-          </div>
-          <div class="block-item-content">
-            <p>{{ item.description }}</p>
-            <span 
-              v-if="item.info" 
-              class="info-trigger" 
-              @click.stop="toggleTooltip(`${blockIndex}-${itemIndex}`)"
-              aria-label="Подробнее"
-            >
-              <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <circle cx="12" cy="12" r="10" />
-                <path d="M12 16v-4" />
-                <path d="M12 8h.01" />
-              </svg>
-            </span>
-            <div 
-              v-show="activeTooltip === `${blockIndex}-${itemIndex}`" 
-              class="tooltip"
-              @click.stop
-            >
-              {{ item.info }}
+    <div v-for="(block, blockIndex) in filteredBlocks" :key="blockIndex" class="block-section">
+      <div v-if="block.type === 'cubes'" :class="`block-type-${type}`">
+        <div class="block-title">{{ block.name }}</div>
+        <div class="block-grid">
+          <div v-for="(item, itemIndex) in block.list" :key="itemIndex" class="block-item">
+            <div class="block-item-image">
+              <img :src="item.image" loading="lazy" />
             </div>
+            <div class="block-item-content">
+              <p>{{ item.point }}</p>
+              <span v-if="item.info" class="info-trigger" @click.stop="toggleTooltip(`${blockIndex}-${itemIndex}`)" aria-label="Подробнее">
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                  <circle cx="12" cy="12" r="10" />
+                  <path d="M12 16v-4" />
+                  <path d="M12 8h.01" />
+                </svg>
+              </span>
+              <div v-show="activeTooltip === `${blockIndex}-${itemIndex}`" class="tooltip" @click.stop>{{ item.info }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-else-if="block.type === 'social'" :class="`block-type-${type}`">
+        <div class="background">
+          <div class="block-title">{{ block.name }}</div>
+        </div>
+        <div class="block-grid">
+          <div class="block-item">
+            <div class="block-item-links">
+              <div v-for="(item, itemIndex) in block.list" :key="itemIndex" class="block-item-links-elem">
+                <img :src="item.image" loading="lazy" />
+              </div>
+            </div>
+          </div>
+          <div class="block-item">
+            <img :src="block.image" loading="lazy" />
           </div>
         </div>
       </div>
@@ -43,7 +51,7 @@ import { useUrlMedia } from '../utils/useUrlMedia'
 const props = defineProps({
   type: {
     type: String,
-    default: 'kvadrat'
+    default: 'default'
   }
 })
 const { frontmatter } = useData()
@@ -53,6 +61,7 @@ const filteredBlocks = computed(() => {
   const matchedBlocks = rawBlocks.filter(block => block.type === props.type)
   return matchedBlocks.map(block => ({
     ...block,
+    image: block.image ? useUrlMedia(block.image, 'support', '', 'image') : '',
     list: block.list?.map(item => ({
       ...item,
       image: item.image ? useUrlMedia(item.image, 'support', '', 'image') : ''
