f06270350ea9391cf05a53acd35d27d24fa4cf0f
mikhaildadaev <mikhaildadaev@gmail.com>
2026-07-22 20:28:56 +0300

    Обновление компонента

--- .vitepress/components/StyleBlockPage.vue
+++ .vitepress/components/StyleBlockPage.vue
@@ -25,13 +25,7 @@
             </div>
             <div class="content">
               <h6 class="name">{{ item.name }}</h6>
-              <div v-if="item.info" class="info" @click.stop="toggleTooltip(`${blockIndex}-${itemIndex}`)" aria-label="Подробнее">
-                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                  <circle cx="12" cy="12" r="10" />
-                  <path d="M12 16v-4" />
-                  <path d="M12 8h.01" />
-                </svg>
-              </div>
+              <div v-if="item.info" class="info" @click.stop="toggleTooltip(`${blockIndex}-${itemIndex}`)"></div>
               <div v-show="activeTooltip === `${blockIndex}-${itemIndex}`" class="tooltip" @click.stop>{{ item.info }}</div>
             </div>
           </div>
