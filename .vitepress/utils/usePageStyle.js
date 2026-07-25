144c53d0443a73d27d1400cf36e42b36d2d19cf9
mikhaildadaev <mikhaildadaev@gmail.com>
2026-07-25 23:00:32 +0300

    Обновление сриптов

--- .vitepress/utils/usePageStyle.js
+++ .vitepress/utils/usePageStyle.js
@@ -1,95 +1,95 @@
 export const pageImages = {
   '/': {
-    src: '/assets/webp/main.webp',
+    src: '/assets/webp/page_main.webp',
     width: 320,
     float: 'center'
   },
   
   '/about/contact': {
-    src: '/assets/webp/contact.webp',
+    src: '/assets/webp/page_contact.webp',
     width: 320,
     float: 'left'
   },
   '/about/mission': {
-    src: '/assets/webp/mission.webp',
+    src: '/assets/webp/page_mission.webp',
     width: 320,
     float: 'right'
   },
   '/about/tech': {
-    src: '/assets/webp/tech.webp',
+    src: '/assets/webp/page_tech.webp',
     width: 320,
     float: 'center'
   },
 
   '/chance/favorites': {
-    src: '/assets/webp/favorites.webp',
+    src: '/assets/webp/page_favorites.webp',
     width: 320,
     float: 'center'
   },
 
   '/dobrology/join': {
-    src: '/assets/webp/join.webp',
+    src: '/assets/webp/page_join.webp',
     width: 320,
     float: 'left'
   },
   '/dobrology/support': {
-    src: '/assets/webp/support.webp',
+    src: '/assets/webp/page_support.webp',
     width: 320,
     float: 'right'
   },
 
   '/humans/volunteers': {
-    src: '/assets/webp/volunteers.webp',
+    src: '/assets/webp/page_volunteers.webp',
     width: 320,
     float: 'center'
   },
 
   '/organizations/shelters': {
-    src: '/assets/webp/shelters.webp',
+    src: '/assets/webp/page_shelters.webp',
     width: 320,
     float: 'center'
   },
 
   '/pets/cats': {
-    src: '/assets/webp/cats.webp',
+    src: '/assets/webp/page_cats.webp',
     width: 320,
     float: 'left'
   },
   '/pets/dogs': {
-    src: '/assets/webp/dogs.webp',
+    src: '/assets/webp/page_dogs.webp',
     width: 320,
     float: 'right'
   },
   
   '/recommendations/go': {
-    src: '/assets/webp/go.webp',
+    src: '/assets/webp/page_go.webp',
     width: 320,
     float: 'center'
   },
   '/recommendations/health': {
-    src: '/assets/webp/health.webp',
+    src: '/assets/webp/page_health.webp',
     width: 320,
     float: 'left'
   },
   '/recommendations/trouble': {
-    src: '/assets/webp/trouble.webp',
+    src: '/assets/webp/page_trouble.webp',
     width: 320,
     float: 'right'
   },
   '/recommendations/welfare': {
-    src: '/assets/webp/welfare.webp',
+    src: '/assets/webp/page_welfare.webp',
     width: 320,
     float: 'center'
   },
   
   default: {
-    src: '/assets/webp/default.webp',
+    src: '/assets/webp/page_default.webp',
     width: 320,
     float: 'center'
   }
 }
 
-export function useStylePage(path) {
+export function usePageStyle(path) {
   const cleanPath = path.replace(/^\/petly/, '').replace(/\/$/, '')
   if (!cleanPath || cleanPath === '/') {
     return pageImages['/']
