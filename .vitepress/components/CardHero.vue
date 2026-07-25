52aa17bbee2748527df9765b8353745f04980ff8
mikhaildadaev <mikhaildadaev@gmail.com>
2026-07-26 01:36:54 +0300

    Обновление компонента

--- .vitepress/components/CardHero.vue
+++ .vitepress/components/CardHero.vue
@@ -2,10 +2,14 @@
   <div v-if="config && config.fields" class="aspect-card hero-card">
     <div class="hero-meta">
       <template v-for="displayField in config.fields.display" :key="displayField">
-        <label v-if="item && item[displayField]" :class="`tag ${displayField.replace('Display', '')}-tag`">{{ item[displayField] }}</label>
+        <label v-if="item && item[displayField]" :class="`tag ${displayField.replace('Display', '')}-tag`" :data-gender="displayField === 'genderDisplay' ? item.gender : null">{{ item[displayField] }}</label>
       </template>
     </div>
-    <img :src="item.image || ''" class="hero-image" loading="lazy" />
+    <picture>
+      <source :srcset="item.imageVertical || ''" media="(max-width: 735px)" />
+      <source :srcset="item.imageHorizontal || item.imageVertical || ''" media="(min-width: 736px)" />
+      <img :src="item.imageHorizontal || item.imageVertical || ''" class="hero-image" loading="lazy" />
+    </picture>
     <div :class="['hero-overlay', useRandomClass(item.uuid || '')]">
       <h1 class="title">{{ item.nameDisplay || '' }}</h1>
       <button v-if="type === 'pets' && item.uuid" class="favorite" :class="{ 'is-favorite': isFavorite }" @click.stop="toggleFavorite(item.uuid)" :title="translate('ui', 'Добавить в избранное')">
@@ -30,11 +34,8 @@ import { useUrlMedia } from '../utils/useUrlMedia'
 export default {
   name: 'CardHero',
   props: {
-    type: {
-      type: String,
-      required: true,
-      validator: (value) => ['humans', 'organizations', 'pets'].includes(value)
-    }
+    type: { type: String, required: true },
+    itemType: { type: String, required: true }
   },
   setup(props) {
     const { lang, frontmatter } = useData()
@@ -43,25 +44,28 @@ export default {
     const { useRandomClass } = useRandomColor()
     const { isFavorite, toggleFavorite, checkIsFavorite } = useFavorites()
     const fm = computed(() => frontmatter.value || {})
-    const isMobile = ref(false)
-    const checkMobile = () => {
-      if (typeof window !== 'undefined') {
-        isMobile.value = window.innerWidth < 735
+    const transformItem = (data) => {
+      const base = {
+        uuid: data.uuid,
+        nameDisplay: data.title || '',
+        descriptionDisplay: data.description || '',
+        covenantID: data.covenantID || '',
+        imageHorizontal: useUrlMedia(
+          data.image?.find(img => img.horizontal)?.horizontal || '',
+          'image'
+        ),
+        imageVertical: useUrlMedia(
+          data.image?.find(img => img.vertical)?.vertical || '',
+          'image'
+        ),
+        type: props.itemType,
+        ...(config.transform ? config.transform(data, lang.value, translate) : {})
       }
+      return base
     }
     const item = computed(() => {
       const data = fm.value || {}
-      const uuid = data.uuid || ''
-      const images = data.image || []
-      let image = isMobile.value ? images.find(img => img.vertical)?.vertical || '' : images.find(img => img.horizontal)?.horizontal || ''
-      const transformed = config && config.transform ? config.transform(data, lang.value, translate) : {}
-      return {
-        uuid,
-        nameDisplay: data.title || '',
-        descriptionDisplay: data.description || '',
-        image: useUrlMedia(image, 'image'),
-        ...transformed
-      }
+      return transformItem(data)
     })
     const checkFavoriteStatus = (uuid) => {
       if (uuid) {
@@ -71,12 +75,9 @@ export default {
     let resizeTimeout = null
     const handleResize = () => {
       if (resizeTimeout) clearTimeout(resizeTimeout)
-      resizeTimeout = setTimeout(() => {
-        checkMobile()
-      }, 100)
+      resizeTimeout = setTimeout(() => { resizeTimeout = null }, 100)
     }
     onMounted(() => {
-      checkMobile()
       if (typeof window !== 'undefined') {
         window.addEventListener('resize', handleResize)
       }
