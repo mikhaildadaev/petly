f4597e3b06270278a3991c250af5a8859bbd5c35
mikhaildadaev <mikhaildadaev@gmail.com>
2026-07-27 00:30:01 +0300

    Обновление компонента

--- .vitepress/components/BlockStyle.vue
+++ .vitepress/components/BlockStyle.vue
@@ -47,6 +47,7 @@
                 <a v-if="key === 'phone'" :href="`tel:${String(value).replace(/[^0-9+]/g, '')}`">{{ value }}</a>
                 <a v-else-if="key === 'email'" :href="`mailto:${value}`">{{ value }}</a>
                 <a v-else-if="key === 'site'" :href="String(value).startsWith('http') ? value : `https://${value}`" target="_blank">{{ value }}</a>
+                <a v-else-if="key === 'telegram'" :href="`https://t.me/${String(value).replace('@', '')}`" target="_blank" rel="noopener">{{ value }}</a>
                 <span v-else>{{ value }}</span>
               </span>
             </div>
